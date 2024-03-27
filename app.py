1463a871ce9292d3c1bcab74de9659bce26ba2ed
Nick088 <Nick088@users.noreply.huggingface.co>
2024-03-27 11:48:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -61,9 +61,10 @@ def inference_image(image, size):
 
 def inference_video(video, size):
     _id = randint(1, 10000)
-    INPUT_DIR = "tmp/" + str(_id) + "/"
+    INPUT_DIR = "/tmp/" + str(_id) + "/"
     os.system("rm -rf " + INPUT_DIR)
     os.system("mkdir " + INPUT_DIR)
+    os.system(f"cd {INPUT_DIR")
     upload_folder = 'upload'
     result_folder = 'results'
     video_folder = 'videos'
